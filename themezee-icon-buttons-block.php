337d00ab14468047043baec16600dd9235435864
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-10-27 14:07:41 +0200

    added license settings and plugin updater

--- themezee-icon-buttons-block.php
+++ themezee-icon-buttons-block.php
@@ -25,3 +25,51 @@ function register_themezee_icon_buttons_block() {
 	register_block_type( __DIR__ . '/build/icon-buttons' );
 }
 add_action( 'init', 'register_themezee_icon_buttons_block' );
+
+
+/**
+ * Set up the Plugin Updater Constants.
+ */
+define( 'THEMEZEE_ICON_BUTTONS_BLOCK_VERSION', '1.0' );
+define( 'THEMEZEE_ICON_BUTTONS_BLOCK_NAME', 'Icon Buttons Block' );
+define( 'THEMEZEE_ICON_BUTTONS_BLOCK_ID', 89731 );
+define( 'THEMEZEE_ICON_BUTTONS_BLOCK_STORE_URL', 'https://themezee.com' );
+
+/**
+ * Include License Settings and Plugin Updater.
+ */
+include dirname( __FILE__ ) . '/includes/class-themezee-blocks-admin-page.php';
+include dirname( __FILE__ ) . '/includes/class-themezee-icon-buttons-block-license-settings.php';
+include dirname( __FILE__ ) . '/includes/class-themezee-icon-buttons-block-updater.php';
+
+
+/**
+ * Initialize the updater. Hooked into `init` to work with the
+ * wp_version_check cron job, which allows auto-updates.
+ */
+function update_themezee_icon_buttons_block() {
+
+	// To support auto-updates, this needs to run during the wp_version_check cron job for privileged users.
+	$doing_cron = defined( 'DOING_CRON' ) && DOING_CRON;
+	if ( ! current_user_can( 'manage_options' ) && ! $doing_cron ) {
+		return;
+	}
+
+	// Retrieve our license key from the DB.
+	$options     = get_option( 'themezee_blocks_settings', array() );
+	$license_key = ! empty( $options['icon_buttons_block_license_key'] ) ? trim( $options['icon_buttons_block_license_key'] ) : false;
+
+	// Setup the updater.
+	$edd_updater = new ThemeZee_Icon_Buttons_Block_Updater(
+		THEMEZEE_ICON_BUTTONS_BLOCK_STORE_URL,
+		__FILE__,
+		array(
+			'version' => THEMEZEE_ICON_BUTTONS_BLOCK_VERSION,
+			'license' => $license_key,
+			'item_id' => THEMEZEE_ICON_BUTTONS_BLOCK_ID,
+			'author'  => 'ThemeZee',
+			'beta'    => false,
+		)
+	);
+}
+add_action( 'init', 'update_themezee_icon_buttons_block' );
